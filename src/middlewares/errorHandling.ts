c10dea7b72731094b1242240f68aea9064bdbf0b
wmarquesv <walace.mv@hotmail.com>
2026-05-10 21:24:41 -0300

    adicionado tratamento de exceções com zod

--- src/middlewares/errorHandling.ts
+++ src/middlewares/errorHandling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/appError";
+import { ZodError } from "zod";
 
 export function errorHandling(
   error: any,
@@ -9,6 +10,13 @@ export function errorHandling(
 ) {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      message: "validation error",
+      issues: error.format(),
+    });
   }
 
   return response.status(500).json({ message: error.message });
