c451745d80df838691fd24916a9ca1a362d4beef
wmarquesv <walace.mv@hotmail.com>
2026-05-11 12:11:16 -0300

    middleware de autenticação criado

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/appError";
+
+interface TokenPayload {
+  role: string;
+  sub: string;
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError("JWT token not found.", 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    const { role, sub: user_id } = verify(
+      token,
+      authConfig.jwt.secret
+    ) as TokenPayload;
+
+    request.user = {
+      id: user_id,
+      role,
+    };
+
+    return next();
+  } catch (error) {
+    throw new AppError("Invalid JWT token.", 401);
+  }
+}
