adc5a1e2ce286c28ba8babb4c6dc259ace88f837
wmarquesv <walace.mv@hotmail.com>
2026-05-12 00:23:31 -0300

    exibição de detalhes do pedido, adicionado logica de autorizações

--- src/controllers/deliveryLogsController.ts
+++ src/controllers/deliveryLogsController.ts
@@ -33,6 +33,27 @@ class DeliveryLogsController {
 
     return response.status(201).json();
   }
+
+  async show(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      delivery_id: z.string().uuid(),
+    });
+
+    const { delivery_id } = paramsSchema.parse(request.params);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    if (
+      request.user?.role === "customer" &&
+      request.user.id !== delivery?.userId
+    ) {
+      throw new AppError("the user can only view their deliveries", 401);
+    }
+
+    return response.json(delivery);
+  }
 }
 
 export { DeliveryLogsController };
--- src/routes/deliveriesLogsRoutes.ts
+++ src/routes/deliveriesLogsRoutes.ts
@@ -14,5 +14,11 @@ deliveryLogsRoutes.post(
   verifyUserAuthorization(["sale"]),
   deliveryLogsController.create
 );
+deliveryLogsRoutes.get(
+  "/:delivery_id/show",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale", "customer"]),
+  deliveryLogsController.show
+);
 
 export { deliveryLogsRoutes };
