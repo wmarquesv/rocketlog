12930277b1d496fc81490630069bbe4f8328a516
wmarquesv <walace.mv@hotmail.com>
2026-05-12 18:33:00 -0300

    adicionado teste automatizado para criação de usuário

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   testEnvironment: "node",
   testMatch: ["<rootDir>/src/**/*.test.ts"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir/src/$1>",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 };
 
--- src/tests/usersController.test.ts
+++ src/tests/usersController.test.ts
@@ -1,5 +1,26 @@
+import request from "supertest";
+import { prisma } from "@/database/prisma";
+
+import { app } from "@/app";
+
 describe("UsersController", () => {
-  it("test", () => {
-    console.log("Passou por aqui!");
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
+
+  it("should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id;
   });
 });
