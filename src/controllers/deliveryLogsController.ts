bf2b85d7c277c42401d1ac65572598f575845cba
wmarquesv <walace.mv@hotmail.com>
2026-05-12 00:14:26 -0300

    implementado rastreio do pedido com os logs do mesmo

--- src/controllers/deliveryLogsController.ts
+++ src/controllers/deliveryLogsController.ts
@@ -1,8 +1,37 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/appError";
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    if (!delivery) {
+      throw new AppError("Delivery not found", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped");
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
