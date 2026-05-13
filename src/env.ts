bcb7ed85961b1fd04607175b4f254f5e1d03a31f
wmarquesv <walace.mv@hotmail.com>
2026-05-13 00:13:43 -0300

    preparando ambiente para deploy: configurado porta do projeto

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
+  PORT: z.coerce.number().default(3333),
 });
 
 export const env = envSchema.parse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "@/app";
+import { env } from "./env";
 
-const PORT = 3333;
+const PORT = env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
